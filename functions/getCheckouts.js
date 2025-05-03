4d1a38782ed816bf253885ad4dc01bed87a6d59d
PatioRatio <scottzm@gmail.com>
2025-05-03 09:57:21 -0700

    Fix netlify errors

--- functions/getCheckouts.js
+++ functions/getCheckouts.js
@@ -1,28 +1,28 @@
 // functions/getCheckouts.js
-const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
   const { cardNumber, pin, accountId } = event.queryStringParameters;
 
-  // 1. login
+  // 1️. Login (cookies kept server-side)
   await fetch(
-    'https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences',
+    "https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences",
     {
-      method: 'POST',
-      headers: {'Content-Type':'application/x-www-form-urlencoded'},
-      body: new URLSearchParams({name: cardNumber, user_pin: pin}),
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({ name: cardNumber, user_pin: pin }),
     }
   );
 
-  // 2. fetch checkouts
-  const url = `https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts`
-    + `?accountId=${accountId}&size=100&page=1&status=OUT&sort=status&locale=en-US`;
-  const res = await fetch(url, {headers:{Accept:'application/json'}});
-  const data = await res.json();
+  // 2️. Grab checkouts
+  const url =
+    `https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts` +
+    `?accountId=${accountId}&size=100&page=1&status=OUT&sort=status&locale=en-US`;
+  const resp = await fetch(url, { headers: { Accept: "application/json" } });
+  const data = await resp.json();
 
   return {
     statusCode: 200,
-    headers: {'Access-Control-Allow-Origin':'*'},
+    headers: { "Access-Control-Allow-Origin": "*" },
     body: JSON.stringify(data),
   };
 };
