b453829cb43c9e6fc6b619b35b12f5bc8c805cfd
PatioRatio <scottzm@gmail.com>
2025-05-03 10:46:13 -0700

    propagate cookies

--- functions/getCheckouts.js
+++ functions/getCheckouts.js
@@ -1,26 +1,30 @@
 // functions/getCheckouts.js
 
+const fetch = require('node-fetch');
+const { CookieJar } = require('tough-cookie');
+const fetchCookie = require('fetch-cookie/node-fetch');
+
+const jar = new CookieJar();
+const fetchWithCookies = fetchCookie(fetch, jar);
+
 const LOGIN_URL     = "https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences";
 const CHECKOUTS_URL = "https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts";
 
 exports.handler = async (event) => {
   const { name, user_pin, accountId } = event.queryStringParameters;
 
-  // 1️⃣ Login (cookies stored in the Function’s context)
-  await fetch(LOGIN_URL, {
+  // 1️⃣ Perform login; cookie jar captures the session cookie
+  await fetchWithCookies(LOGIN_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
     body: new URLSearchParams({ name, user_pin })
   });
 
-  // 2️⃣ Fetch checkouts
-  const url = CHECKOUTS_URL
-    + `?accountId=${accountId}`
-    + `&size=100&page=1&status=OUT&sort=status&locale=en-US`;
-  const resp = await fetch(url, { headers: { Accept: 'application/json' } });
+  // 2️⃣ Fetch checkouts using the same cookie jar
+  const url = `${CHECKOUTS_URL}?accountId=${accountId}&size=100&page=1&status=OUT&sort=status&locale=en-US`;
+  const resp = await fetchWithCookies(url, { headers: { Accept: 'application/json' } });
   const data = await resp.json();
 
-  // 3️⃣ Return with CORS header
   return {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
