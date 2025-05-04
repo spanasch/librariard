19b7fed606c6e0e66775681f06a98fb53d12a61c
Scott Pari-Monasch <scottzm@gmail.com>
2025-05-03 19:18:00 -0700

    move top-level await

--- functions/getCheckouts.js
+++ functions/getCheckouts.js
@@ -1,38 +1,27 @@
-// functions/getCheckouts.js
-
-const { CookieJar } = await import('tough-cookie');
-const fetchModule = await import('node-fetch');
-const fetchCookieModule = await import('fetch-cookie');
-const fetch = fetchModule.default;
-const fetchCookie = fetchCookieModule.default;
-
-const jar = new CookieJar();
-const fetchWithCookies = fetchCookie(fetch, jar);
-
-const LOGIN_URL     = "https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences";
-const CHECKOUTS_URL = "https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts";
-
 exports.handler = async (event) => {
   const { CookieJar } = await import('tough-cookie');
   const fetchModule = await import('node-fetch');
   const fetchCookieModule = await import('fetch-cookie');
+
   const fetch = fetchModule.default;
   const fetchCookie = fetchCookieModule.default;
 
   const jar = new CookieJar();
   const fetchWithCookies = fetchCookie(fetch, jar);
 
-  const { name, user_pin, accountId } = event.queryStringParameters;
-
-  const LOGIN_URL     = "https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences";
+  const LOGIN_URL = "https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences";
   const CHECKOUTS_URL = "https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts";
 
+  const { name, user_pin, accountId } = event.queryStringParameters;
+
+  // Step 1: login
   await fetchWithCookies(LOGIN_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
     body: new URLSearchParams({ name, user_pin })
   });
 
+  // Step 2: get checkouts
   const url = `${CHECKOUTS_URL}?accountId=${accountId}&size=100&page=1&status=OUT&sort=status&locale=en-US`;
   const resp = await fetchWithCookies(url, { headers: { Accept: 'application/json' } });
   const data = await resp.json();
@@ -43,4 +32,3 @@ exports.handler = async (event) => {
     body: JSON.stringify(data),
   };
 };
-
