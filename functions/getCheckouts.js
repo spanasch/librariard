af2fea706601311d1ed1170d0c4391071165be22
Scott Pari-Monasch <scottzm@gmail.com>
2025-05-03 19:11:53 -0700

    dynamic import instead of require

--- functions/getCheckouts.js
+++ functions/getCheckouts.js
@@ -1,8 +1,10 @@
 // functions/getCheckouts.js
 
-const fetch = require('node-fetch');
-const { CookieJar } = require('tough-cookie');
-const fetchCookie = require('fetch-cookie/node-fetch');
+const { CookieJar } = await import('tough-cookie');
+const fetchModule = await import('node-fetch');
+const fetchCookieModule = await import('fetch-cookie');
+const fetch = fetchModule.default;
+const fetchCookie = fetchCookieModule.default;
 
 const jar = new CookieJar();
 const fetchWithCookies = fetchCookie(fetch, jar);
@@ -11,16 +13,26 @@ const LOGIN_URL     = "https://aclibrary.bibliocommons.com/user/login?destinatio
 const CHECKOUTS_URL = "https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts";
 
 exports.handler = async (event) => {
+  const { CookieJar } = await import('tough-cookie');
+  const fetchModule = await import('node-fetch');
+  const fetchCookieModule = await import('fetch-cookie');
+  const fetch = fetchModule.default;
+  const fetchCookie = fetchCookieModule.default;
+
+  const jar = new CookieJar();
+  const fetchWithCookies = fetchCookie(fetch, jar);
+
   const { name, user_pin, accountId } = event.queryStringParameters;
 
-  // 1️⃣ Perform login; cookie jar captures the session cookie
+  const LOGIN_URL     = "https://aclibrary.bibliocommons.com/user/login?destination=%2Faccount%2Fcontact_preferences";
+  const CHECKOUTS_URL = "https://gateway.bibliocommons.com/v2/libraries/aclibrary/checkouts";
+
   await fetchWithCookies(LOGIN_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
     body: new URLSearchParams({ name, user_pin })
   });
 
-  // 2️⃣ Fetch checkouts using the same cookie jar
   const url = `${CHECKOUTS_URL}?accountId=${accountId}&size=100&page=1&status=OUT&sort=status&locale=en-US`;
   const resp = await fetchWithCookies(url, { headers: { Accept: 'application/json' } });
   const data = await resp.json();
@@ -31,3 +43,4 @@ exports.handler = async (event) => {
     body: JSON.stringify(data),
   };
 };
+
